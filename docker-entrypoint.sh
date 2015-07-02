ebe8c58e112d0c7e91447abf175a912dc225298e
Kevin Pettijohn <pettijohn.k@gmail.com>
2015-07-01 23:16:47 -0700

    Fix sed command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = 'statsite' ]; then
   if [ "$SINK" ]; then
-    sed -ri "s/^(stream_cmd = )\S+/\1$SINK/" /etc/statsite/statsite.conf
+    sed -ri "s,^(stream_cmd = )\S+,\1$SINK," /etc/statsite/statsite.conf
   fi
   exec statsite -f /etc/statsite/statsite.conf
 fi
